f45e85b6e502ef793b52db50a024817cba29783f
ODAYMAHMOUD111 <odaymahmoud443@gmail.com>
2025-10-24 19:19:50 +0300

    Add files via upload
    c# tasks

---
+++ csharp/day5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "oday", age = "19", country = "egypt";
+            Console.WriteLine($"hello mr {name}" + " " + "your age is " + "\"" +  age + "\" " +"your country is " +  country + "\"");
+
+
+        }
+    }
+}
