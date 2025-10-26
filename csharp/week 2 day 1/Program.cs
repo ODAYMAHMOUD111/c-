c5978d156c2f4f05fbf60e62ed9e466eb607f026
ODAYMAHMOUD111 <odaymahmoud443@gmail.com>
2025-10-26 18:32:01 +0300

    Add files via upload
    c # task

---
+++ csharp/week 2 day 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float radius, area;
+            const float pi = 3.14f;
+            Console.WriteLine("enter the radius of ball ");
+             radius = Convert.ToSingle(Console.ReadLine());
+            area = 4*pi*radius*radius;
+            Console.WriteLine("the area of ball = {0} ", area);
+          
+
+
+
+
+
+        }  
+    }
+}
